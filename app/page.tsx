885efb76c95277b1c9ac3bd1e481d9730f1d3b56
Myroslava Kukhta <miroslavakukhta@gmail.com>
2024-09-13 17:56:50 +0300

    Add nav and home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,48 @@
 "use client"
 
+import Link from "next/link"
+
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
-    </div>
-  );
+export default function Home() {
+    return (
+        <div className="bg-green-100 min-h-screen flex flex-col items-center py-10">
+            <h1 className="text-5xl font-bold text-green-800 mb-10">
+                Welcome Home!
+            </h1>
+            <p className="text-green-700 text-lg mb-6 text-center maxx-w-2xl">
+                Step into the magical home of Glenda the Witch. Here, you can explore her mystical world, create potions
+                in her cauldron, browse through her spellbook
+            </p>
+
+            {/*<div className="mb-8">*/}
+            {/*    <Image*/}
+            {/*        src="/witch-house.jpg"*/}
+            {/*        className="rounded shadow-lg"*/}
+            {/*        alt="Witch house"*/}
+            {/*        width={600}*/}
+            {/*        height={400}>*/}
+            {/*        </Image>*/}
+            {/*</div>*/}
+
+            <nav className="mb-10">
+                <ul className="space-y-4">
+                    <li>
+                        <Link href="/cauldron" className="text-2xl font-bold text-green-900 hover:underline">
+                            Cauldron
+                        </Link>
+
+                        <Link href="/spellbook" className="text-2xl font-bold text-purple-900 hover:underline">
+                                Spellbook
+                        </Link>
+
+                        <Link href="/potions" className="text-2xl font-bold text-blue-900 hover:underline">
+                                Potions
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+);
 }
